7d0d77e03f11f3af0aa39c2f3c499c7ae9718499
ellecavalli <56439288+ellecavalli@users.noreply.github.com>
2020-11-23 12:24:16 +0100

    first try timeline

--- assets/js/main.js
+++ assets/js/main.js
@@ -144,4 +144,18 @@
     aos_init();
   });
 
+  // Timeline details
+  $(function(){
+    var inputs = $('.input');
+    var paras = $('.description-flex-container').find('p');
+    inputs.click(function(){
+      var t = $(this),
+          ind = t.index(),
+          matchedPara = paras.eq(ind);
+
+      t.add(matchedPara).addClass('active');
+      inputs.not(t).add(paras.not(matchedPara)).removeClass('active');
+    });
+  });
+
 })(jQuery);
